e06a33ee4fc6bf51e9a4fa8b8ceea92d4639ffc0
Sachi Hirani <sachihirani@Sachis-MacBook-Pro.local>
2025-05-07 18:35:42 +1000

    Made small update to Streamlit app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime
+from datetime import datetime, timedelta
 from PIL import Image
 
 # --- Config ---
